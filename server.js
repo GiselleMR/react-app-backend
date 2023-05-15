c3e7aa3e9b0c7655e7701be529a381e4b8e5ca3d
GiselleMR <giselle.rubin1@gmail.com>
2023-05-15 10:53:48 -0400

    define crud routes for dogs

--- server.js
+++ server.js
@@ -4,11 +4,33 @@ const port = 3000;
 
 
 
-//Routes
-app.get('/', (res, req) => {
+//CRUD Routes RESTful
+
+//GET all dogs
+app.get('/dogs', (req, res) => {
     res.setEncoding('Hello World');
-})
+});
+
+//POST create a dog
+app.post('/dogs', (req, res) => {
+    res.send('This is the POST route for dogs');
+});
+
+//GET one dog
+app.get('/dogs/:id', (req, res) => {
+    res.send('this is the GET route for one dog');
+});
+
+//PUT one dog
+app.put('/dogs/:id', (req, res) => {
+    res.send('This is the PUT route for one dog');
+});
+
+//DELETE one dong
+app.delete('/dogs/:id', (req, res) => {
+    res.send('This is the DELETE route for one dog');
+});
 
 app.listen(port, () => {
     console.log('You are connected to the port 3000');
-})
+});
