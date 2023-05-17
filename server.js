ab8bb8b03648476a2c7931d24221a2945242a922
GiselleMR <giselle.rubin1@gmail.com>
2023-05-17 10:26:17 -0400

    configure db connectin and add db methods

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const dogsRouter = require('./routes/dogs')
 
+//environment
+require('dotenv').config;
 
+//database connection
+require('./config/database');
 
-//CRUD Routes RESTful
+//Middleware
+app.use(express.urlencoded({extended: true}));
 
-//GET all dogs
-app.get('/dogs', (req, res) => {
-    res.setEncoding('Hello World');
-});
-
-//POST create a dog
-app.post('/dogs', (req, res) => {
-    res.send('This is the POST route for dogs');
-});
-
-//GET one dog
-app.get('/dogs/:id', (req, res) => {
-    res.send('this is the GET route for one dog');
-});
-
-//PUT one dog
-app.put('/dogs/:id', (req, res) => {
-    res.send('This is the PUT route for one dog');
-});
-
-//DELETE one dong
-app.delete('/dogs/:id', (req, res) => {
-    res.send('This is the DELETE route for one dog');
-});
+// ROUTES
+app.use('/dogs', dogsRouter)
 
 app.listen(port, () => {
     console.log('You are connected to the port 3000');
